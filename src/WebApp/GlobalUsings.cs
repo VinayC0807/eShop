811f2790296713f148d76133be714c91a879065e
vinay-ecanarys <123@gmail.com>
2025-10-15 12:42:40 +0530

    Fix compilation errors - remove conflicting Program files and exclude EventBus dependencies

--- src/WebApp/GlobalUsings.cs
+++ src/WebApp/GlobalUsings.cs
@@ -1,3 +1,3 @@
 ﻿global using eShop.WebApp.Components;
 global using eShop.WebApp.Services;
-global using eShop.ServiceDefaults;
+// global using eShop.ServiceDefaults; // Removed for standalone version
